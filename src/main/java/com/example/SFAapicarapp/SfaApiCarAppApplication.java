ea2de14ea7f46108e1baee4b9b00b6de39fc7fe0
SlawomirWenc <slawomirwenc@gmail.com>
2021-05-17 15:19:47 +0200

    Initial commit

---
+++ src/main/java/com/example/SFAapicarapp/SfaApiCarAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SFAapicarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfaApiCarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfaApiCarAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SFAapicarapp/SfaApiCarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SFAapicarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfaApiCarAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
